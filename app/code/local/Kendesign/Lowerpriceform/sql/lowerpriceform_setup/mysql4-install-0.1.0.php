d34af26183767e760964bb4013b139194167ba76
kenzoj <ep@kendesign.it>
2013-08-22 19:10:12 +0200

    Corrected error on a mysql query

--- app/code/local/Kendesign/Lowerpriceform/sql/lowerpriceform_setup/mysql4-install-0.1.0.php
+++ app/code/local/Kendesign/Lowerpriceform/sql/lowerpriceform_setup/mysql4-install-0.1.0.php
@@ -7,7 +7,7 @@ $installer->startSetup();
 $installer->run("DROP TABLE IF EXISTS {$this->getTable('lowerpriceform')};
 CREATE TABLE {$this->getTable('lowerpriceform')} (
   `lowerpriceform_id` int(11) unsigned NOT NULL auto_increment,
-  `lower_price_type varchar(12) NOT NULL default '',
+  `lower_price_type` varchar(12) NOT NULL default '',
   `lower_price_url` varchar(255) NOT NULL default '',
   `lower_price_my_price` decimal(7,2) NOT NULL default 0,
   `lower_price_price` decimal(7,2) NOT NULL default 0,
